373e4bb57a4dbf2aeb4e6164f6261414146ffbd3
siddhartha-surnoi <kanaparthisiddhartha@surnoi.com>
2025-11-03 20:53:39 +0530

    Add files via upload

--- main.tf
+++ main.tf
@@ -1,9 +1,35 @@
+# ####################################################
+# # Security Group Module
+# ####################################################
+# module "security_groups" {
+#   source        = "./sg"
+#   vpc_id        = data.aws_vpc.custom.id
+#   allowed_cidr  = var.allowed_cidr
+#   sg_config     = var.security_groups
+#   project_name  = var.project_name
+#   tags          = local.common_tags
+# }
+
+# ####################################################
+# # EC2 Module
+# ####################################################
+# module "ec2_instances" {
+#   source               = "./ec2"
+#   ami_id               = data.aws_ami.devops_team_ami.id
+#   instance_conf        = var.instance_configs
+#   sg_ids               = module.security_groups.sg_ids
+#   subnet_id            = data.aws_subnet.public_subnet.id
+#   project_name         = var.project_name
+#   iam_instance_profile = var.iam_instance_profile
+#   tags                 = local.common_tags
+# }
+
 ####################################################
 # Security Group Module
 ####################################################
 module "security_groups" {
   source        = "./sg"
-  vpc_id        = data.aws_vpc.custom.id
+  vpc_id        = local.vpc_id
   allowed_cidr  = var.allowed_cidr
   sg_config     = var.security_groups
   project_name  = var.project_name
@@ -18,7 +44,7 @@ module "ec2_instances" {
   ami_id               = data.aws_ami.devops_team_ami.id
   instance_conf        = var.instance_configs
   sg_ids               = module.security_groups.sg_ids
-  subnet_id            = data.aws_subnet.public_subnet.id
+  subnet_id            = local.public_subnet_id
   project_name         = var.project_name
   iam_instance_profile = var.iam_instance_profile
   tags                 = local.common_tags
--- terraform.tfvars
+++ terraform.tfvars
@@ -1,5 +1,5 @@
 project_name        = "Logistics"
 environment         = "dev"
-vpc_id              = local.vpc_id
-public_subnet_id    = local.public_subnet_id
+# vpc_id              = local.vpc_id
+# public_subnet_id    = local.public_subnet_id
 #iam_instance_profile = "IAM-ECR-Role"
